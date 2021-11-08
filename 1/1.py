84114956f2c767b77cce60365478f3066f641ccd
IanDs0 <ianhalominecraft@gmail.com>
2021-11-08 15:57:38 -0300

    2

--- 1/1.py
+++ 1/1.py
@@ -1,6 +1,14 @@
 import math
 
-olá=0;
-print (olá);
+olá= int(input('Digite o angulo'));
+print ('O angulo digitado foi:\t',olá);
 
-print(math.cos(olá));
+rad = (olá*3.14)/180;
+
+print("\nrad: ",rad,"\ncos: ",math.cos(olá),"\nsen: ",math.sin(olá),"\ntg: ",math.tan(olá));
+
+import numpy as np
+
+olá= int(input('Digite o angulo'));
+
+print(np.deg2rad(90));
